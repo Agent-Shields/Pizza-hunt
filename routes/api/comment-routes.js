52b112f399e08d79190a6983ee1a5fc571989a7a
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-03-23 22:27:12 -0500

    snapshot

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controllers')
+const { addComment, removeComment } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
-// /api/comments/<pizzaId>/<commendId>
-router.route('/:pizzaId/:commentId').delete(removeComment)
+// /api/comments/<pizzaId>/<commentId>
+router.route('/:pizzaId/:commentId').delete(removeComment);
 
 module.exports = router;
