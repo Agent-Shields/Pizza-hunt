960f860289beed357e91695bbda4193a36005da6
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-03-29 23:48:33 -0500

    had to code snapshot to fix a code issue I had

--- models/comment.js
+++ models/comment.js
@@ -1,47 +1,18 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
-  },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  },
-  replies: [ReplySchema],
-},
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true
-    },
-    // prevents virtuals from creating duplicate of _id as `id`
-    id: false
-  }
-);
-
-// get total reply count 
-CommentSchema.virtual('replyCount').get(function() {
-  return this.replies.length;
-})
-
 const ReplySchema = new Schema(
   {
-    // set cstom id to avoid confusion with parent comment _id 
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String,
+      type: String
     },
     writtenBy: {
-      type: String,
+      type: String
     },
     createdAt: {
       type: Date,
@@ -51,11 +22,39 @@ const ReplySchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
       getters: true
     }
   }
-)
+);
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
 
 const Comment = model('Comment', CommentSchema);
 
